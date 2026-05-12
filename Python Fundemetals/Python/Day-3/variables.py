be9b61d98cf606964b7cc197624b14a167b6690e
Rahul Rudra <rahulrudra665@gmail.com>
2026-05-12 07:47:31 +0600

    python commit

---
+++ Python Fundemetals/Python/Day-3/variables.py
@@ -0,0 +1,11 @@
+price = 2.55
+print(price)
+
+name = "Rahul"
+age = 23
+price = 34.55
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"The price is {price}")
+print(f"The new price  {price}")
